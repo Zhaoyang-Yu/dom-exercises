16aab59fe4ae2c76c9c1380482fd412060dbc46a
Zhaoyang-Yu <zhaoyang.yu@mail.utoronto.ca>
2020-10-27 14:56:09 -0400

    Oct 27, 2:56

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //add your code here
 function runOnLoad () {
     $('h2').css('color','red');
+    $('ul').css('background-color','gold');
+    $('h1').text('DOM Practice for JavaScript');
+    $('li').eq(1).text('random noise');
+    $('article').first().append('<p>I am a paragraph with <em>italics</em> and <strong>bold</strong><p>');
+    $('.instructions').remove();
 }
 
 // "listen" for the "DOMContentLoaded" event, and then run "runOnLoad()"
